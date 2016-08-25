f9fdada57b716189b57dd8e2980507bd9105567f
Jackie Liao <super122388@gmail.com>
2016-08-24 23:04:53 -0700

    was update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ PaperclipDemo::Application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_protocol: 'https',
-    s3_host_name: 's3-us-west-2.amazonaws.com',
+    s3_host_name: 's3-us-west-1.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch("AWS_BUCKET"),
       access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
