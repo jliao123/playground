abffcb1a3afd49cb712de77bfdad76d6ab685491
Jackie Liao <super122388@gmail.com>
2016-08-27 21:18:53 -0700

    Got mandrill working. mod template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < BaseMandrillMailer
   def welcome(email)
-    subject = "Welcome to our awesome app!"
-    send_mail(email, subject, "test body")
+    subject = "New request for quote"
+    merge_vars = {
+      "FIRST_NAME" => "Testy Mc'Tester"
+    }
+    body = mandrill_template("new-space", merge_vars)
+
+    send_mail(user.email, subject, body)
   end
 end
+
