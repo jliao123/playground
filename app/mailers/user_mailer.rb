2fa86ea1310853f5ec522161e855717d809f7ff3
Jackie Liao <super122388@gmail.com>
2016-08-28 20:57:50 -0700

    syntax usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < BaseMandrillMailer
   def welcome(email, img_url)
     subject = "New request for quote"
     merge_vars = {
-      "FIRST_NAME" => "Testy Mc'Tester"
-      "IMG_TEST" => img_url
+      "FIRST_NAME" => "Testy Mc'Tester",
+      "IMG_TEST" => img_url,
     }
     body = mandrill_template("new-space", merge_vars)
 
