8f36bcd820047f78c74fa956b63a4880a4ba7f2e
Jackie Liao <super122388@gmail.com>
2016-09-01 23:23:46 -0700

    Friend.last

--- app/controllers/complete_controller.rb
+++ app/controllers/complete_controller.rb
@@ -1,7 +1,7 @@
 class CompleteController < ApplicationController
   	def index
   		print "I'm here"
-  		@friend = Friend.find(1)
+  		@friend = Friend.last
   		UserMailer.welcome("jackieoliao@gmail.com", @friend.avatar.url(:medium), @friend.name).deliver_now
   	end
 end
