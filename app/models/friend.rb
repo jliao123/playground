bf6c358e07a8eead74fd5143fc21260cb910324f
Jackie Liao <super122388@gmail.com>
2016-09-06 22:30:22 -0700

    took out square + thumbnail for paperclip

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -5,8 +5,8 @@ class Friend < ActiveRecord::Base
     attachment_size: { less_than: 5.megabytes }
 
   has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
+    # thumb: '100x100>',
+    # square: '200x200#',
     medium: '300x300>'
   }
  
