81fdacc64638017c8e5e4c381f24b121f1ec9381
Jackie Liao <super122388@gmail.com>
2016-09-06 22:26:01 -0700

    Added ed's thumbnail + precompiled assets

--- app/controllers/complete_controller.rb
+++ app/controllers/complete_controller.rb
@@ -34,6 +34,6 @@ class CompleteController < ApplicationController
 
 		# UserMailer.welcome("jackieoliao@gmail.com", @friend.avatar.url(:medium), @friend.name).deliver_now
 		InvitationMailer.invite().deliver_now
-		redirect_to '/pdfs/myfile.pdf'
+		# redirect_to '/pdfs/myfile.pdf'
   	end
 end
