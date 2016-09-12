d32c482471b97ac0664279978d2baa418d2eb461
Jackie Liao <super122388@gmail.com>
2016-09-11 21:08:12 -0700

    styling changes

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -64,7 +64,7 @@ class FriendsController < ApplicationController
 
     respond_to do |format|
       if @friend.save
-        format.html { redirect_to root_path, notice: 'Room was successfully added.' }
+        format.html { redirect_to root_path, notice: 'Success! Add more rooms or submit.' }
         format.json { render action: 'show', status: :created, location: @friend }
       else
         format.html { render action: 'new' }
